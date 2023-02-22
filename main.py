e28fd558dad35a004abe66df44d0382fc20da7cd
Jingqian Liu <liujqian@hotmail.com>
2023-02-21 18:04:06 -0800

    update the inference script

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from transformers import AutoTokenizer, AutoModelWithLMHead
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
 tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelWithLMHead.from_pretrained("./fine-tune/GPT2-finetuned-commongen")
-encoder_input_str = "<|endoftext|>airport, airplane, wait="
-input_ids = tokenizer(encoder_input_str, return_tensors="pt").input_ids
+model = AutoModelForCausalLM.from_pretrained("./fine-tune/gpt2-large-finetuned-commongen")
+encoder_input_str = "<|endoftext|> feel, after, doing, housework, hours="
+input = tokenizer(encoder_input_str, return_tensors="pt")
 outputs = model.generate(
-    input_ids,
-    num_beams=10,
-    num_return_sequences=10,
-    remove_invalid_values=True,
-    num_beam_groups=10,
-    diversity_penalty =100.0,
+    **input,
+    # num_beams=10,
+    # num_beam_groups=10,
+    # num_return_sequences=10,
+    # diversity_penalty=100.0,
     length_penalty=0.1,
-    temperature=10.0
+    remove_invalid_values=True,
+    temperature=10.0,
+    max_new_tokens=256,
+    return_dict_in_generate=True,
+    output_scores=True,
 )
+print(type(outputs))
 print("Output:\n" + 100 * '-')
-for output in outputs:
+for output in outputs.sequences:
     print(tokenizer.decode(output, skip_special_tokens=True))
