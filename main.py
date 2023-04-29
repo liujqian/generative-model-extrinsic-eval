f124008f281d71dfffcc812576f060cfbf4be691
Jingqian Liu <liujqian@hotmail.com>
2023-04-29 16:42:29 -0700

    upload some generation results.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, StoppingCriteria, StoppingCriteriaList
 
 language_models = {
     "gpt2-xl": "liujqian/gpt2-xl-finetuned-commongen",
@@ -17,22 +18,37 @@ tokenizers = {
     "t5": "mrm8488/t5-base-finetuned-common_gen",
     "bloom": "mrm8488/bloom-560m-finetuned-common_gen"
 }
+if __name__ == '__main__':
 
-tokenizer = AutoTokenizer.from_pretrained(tokenizers["gpt2"],padding_side='left')
-tokenizer.pad_token = tokenizer.eos_token
-model = AutoModelForCausalLM.from_pretrained(language_models["gpt2"])
-encoder_input_str0 = "<|endoftext|>kid room dance="
-tokenized_input = tokenizer( [encoder_input_str0], return_tensors="pt", padding=True)
-outputs = model.generate(
-    **tokenized_input,
-    num_beams=3,
-    num_return_sequences=1,
-    remove_invalid_values=True,
-    temperature=1,
-    max_new_tokens=256,
-    return_dict_in_generate=True,
-    output_scores=True,
-)
-print("Output:\n" + 100 * '-')
-for output in outputs.sequences:
-    print(tokenizer.decode(output, skip_special_tokens=True))
+    tokenizer = AutoTokenizer.from_pretrained("StabilityAI/stablelm-tuned-alpha-3b")
+    model = AutoModelForCausalLM.from_pretrained("StabilityAI/stablelm-tuned-alpha-3b")
+    model.half().cuda()
+
+
+    class StopOnTokens(StoppingCriteria):
+        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+            stop_ids = [50278, 50279, 50277, 1, 0]
+            for stop_id in stop_ids:
+                if input_ids[0][-1] == stop_id:
+                    return True
+            return False
+
+
+    system_prompt = """<|SYSTEM|># StableLM Tuned (Alpha version)
+    - StableLM is a helpful and harmless open-source AI language model developed by StabilityAI.
+    - StableLM is excited to be able to help the user, but will refuse to do anything that could be considered harmful to the user.
+    - StableLM is more than just an information source, StableLM is also able to write poetry, short stories, and make jokes.
+    - StableLM will refuse to participate in anything that could harm a human.
+    """
+
+    prompt = f'{system_prompt}<|USER|>Create a sentence with the following words: bike, sidewalk, wheel, ride<|ASSISTANT|>'
+
+    inputs = tokenizer(prompt, return_tensors="pt").to("cuda")
+    tokens = model.generate(
+        **inputs,
+        max_new_tokens=64,
+        temperature=0.7,
+        do_sample=True,
+        stopping_criteria=StoppingCriteriaList([StopOnTokens()])
+    )
+    print(tokenizer.decode(tokens[0], skip_special_tokens=True))
