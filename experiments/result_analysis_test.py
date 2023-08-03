ae5bb7cc32d1250f0b1f5f5c4220fba09b16ef6d
Jingqian Liu <liujqian@hotmail.com>
2023-08-02 23:19:44 -0700

    change the way we analyze results. Add some tests.

---
+++ experiments/result_analysis_test.py
@@ -0,0 +1,151 @@
+import unittest
+
+from experiments.results_analysis import count_occurences, get_correct_choice_idx, get_current_question_choice_status_with_choice_analysis, \
+    check_inclusion_scores_predict_correctness, update_subset_stats_for_with_choice_analysis, get_choice_mention_count, \
+    check_no_choice_predict_correctness, update_subset_stats_for_without_choice_analysis
+
+
+def get_sample_question() -> dict:
+    return {'id': 'e8a8b3a2061aa0e6d7c6b522e9612824',
+            'question': 'What home entertainment equipment requires cable?',
+            'question_concept': 'cable',
+            'choices': {'label': ['A', 'B', 'C', 'D', 'E'],
+                        'text': ['radio shack', 'substation', 'cabinet', 'television', 'desk']},
+            'answerKey': 'D',
+            'question_content_words': ['home',
+                                       'entertainment',
+                                       'equipment',
+                                       'require',
+                                       'cable'],
+            'choice_0_content_words': ['radio', 'shack'],
+            'choice_1_content_words': [''],
+            'choice_2_content_words': ['cabinet'],
+            'choice_3_content_words': ['television'],
+            'choice_4_content_words': ['desk']}
+
+
+class TestResultAnalysis(unittest.TestCase):
+    def test_count_occurence(self):
+        sentences = [
+            "I love to go to New York in Germany.",
+            "I hope I can go to top of the world one day. I would love that."
+        ]
+        target = ["love", "top"]
+        self.assertEqual(count_occurences(sentences, target), 3)
+        self.assertEqual(count_occurences(sentences, []), 0)
+        self.assertEqual(count_occurences(sentences, ["New York", "hope"]), 2)
+        self.assertEqual(count_occurences(sentences, ["world"]), 1)
+        self.assertEqual(count_occurences(sentences, ["apple"]), 0)
+        self.assertEqual(count_occurences(sentences, ["germany"]), 1)
+        self.assertEqual(count_occurences(sentences, ["in", ""]), 1)
+
+    def test_get_correct_idx(self):
+        idx = get_correct_choice_idx(get_sample_question())
+        self.assertEqual(idx, 3)
+
+    def test_get_current_question_choice_status_with_choice_analysis(self):
+        generations = [
+            "I love radio shack.", "Radio shack loves me.", "I hate you.", "I play radio.",
+            "I like substation.", "Eat.", "Sleep.", "Drink.",
+            "Hit.", "Drive.", "Poop.", "Beat.",
+            "I watch television.", "I like televisions as a television makes me happy.", "I love televisions.", "I have a television.",
+            "I have a desk.", "I don't eat apples.", "I bought many desks.", "I slept."
+        ]
+        scores = [1, 1, 1, 1, 2, 2, 2, 2, 1, 2, 3, 4, 0, 0, 0, 0, -1, -2, -3, -4]
+        g = {"sentences": generations, "sequences_scores": scores, "id": 1}
+        results = get_current_question_choice_status_with_choice_analysis(question=get_sample_question(), generations=g)
+        self.assertEqual(results["inclusion_count"], [5, 0, 0, 5, 2])
+        self.assertEqual(results["avg_sequences_scores"], [1, 2, 2.5, 0, -2.5])
+
+    def test_check_inclusion_scores_predict_correctness(self):
+        stats = {
+            "inclusion_count": [5, 0, 0, 5, 2],
+            "avg_sequences_scores": [1, 2, 2.5, 2.5, -2.5]
+        }
+        self.assertEqual(check_inclusion_scores_predict_correctness(idx_correct_choice=0, choices_stats=stats), (True, False))
+        self.assertEqual(check_inclusion_scores_predict_correctness(idx_correct_choice=3, choices_stats=stats), (True, True))
+        self.assertEqual(check_inclusion_scores_predict_correctness(idx_correct_choice=4, choices_stats=stats), (False, False))
+
+    def test_update_subset_stats_for_with_choice_analysis(self):
+        stats1 = {
+            "total_examined": 2,
+            "correct_prediction_by_inclusion_count": 78,
+            "correct_prediction_by_sequences_score": 4,
+            "avg_correct_prediction_inclusion": 20,
+            "avg_wrong_prediction_inclusion": 7,
+            "avg_correct_prediction_sequence_score": 2,
+            "avg_wrong_prediction_sequence_score": 2,
+        }
+        choices_stats = {
+            "inclusion_count": [5, 0, 0, 5, 2],
+            "avg_sequences_scores": [1, 2, 2.5, 2.5, -2.5]
+        }
+        update_subset_stats_for_with_choice_analysis(
+            stats=stats1,
+            choices_stats=choices_stats,
+            idx_correct_choice=3
+        )
+        self.assertEqual(stats1["total_examined"], 3)
+        self.assertEqual(stats1["correct_prediction_by_inclusion_count"], 79)
+        self.assertEqual(stats1["correct_prediction_by_sequences_score"], 5)
+        self.assertEqual(stats1["avg_correct_prediction_inclusion"], 15)
+        update_subset_stats_for_with_choice_analysis(
+            stats=stats1,
+            choices_stats=choices_stats,
+            idx_correct_choice=0
+        )
+        self.assertEqual(stats1["total_examined"], 4)
+        self.assertEqual(stats1["correct_prediction_by_inclusion_count"], 80)
+        self.assertEqual(stats1["correct_prediction_by_sequences_score"], 5)
+        update_subset_stats_for_with_choice_analysis(
+            stats=stats1,
+            choices_stats=choices_stats,
+            idx_correct_choice=2
+        )
+        self.assertEqual(stats1["total_examined"], 5)
+        self.assertEqual(stats1["correct_prediction_by_inclusion_count"], 80)
+        self.assertEqual(stats1["correct_prediction_by_sequences_score"], 6)
+        update_subset_stats_for_with_choice_analysis(
+            stats=stats1,
+            choices_stats=choices_stats,
+            idx_correct_choice=4
+        )
+        self.assertEqual(stats1["total_examined"], 6)
+        self.assertEqual(stats1["correct_prediction_by_inclusion_count"], 80)
+        self.assertEqual(stats1["correct_prediction_by_sequences_score"], 6)
+
+    def test_get_choice_mention_count(self):
+        question = get_sample_question()
+        generations = [
+            "I love radio stations.", "I eat shake shack.", 'I have a net', "I have televisions.", "My box is on my desk.",
+            "I have a radio shack.", "I have a radio on my bed.", "My television is in my room.", "I sleep on my television on my desk.",
+            "I have a shack.",
+            "Sleep.", "Eat", "Hit.", "I love!", "I eat apples.",
+            "Play.", "Drive.", "Sing.", "Smile.", "Take it!"
+        ]
+        counts = [6, 0, 0, 3, 2]
+        results = get_choice_mention_count(question, {"sentences": generations})
+        self.assertEqual(counts, results)
+
+    def test_check_no_choice_predict_correctness(self):
+        self.assertTrue(check_no_choice_predict_correctness([6, 6, 0, 3, 2], 0))
+        self.assertTrue(check_no_choice_predict_correctness([6, 6, 0, 3, 2], 1))
+        self.assertTrue(not check_no_choice_predict_correctness([6, 6, 0, 3, 2], 2))
+        self.assertTrue(check_no_choice_predict_correctness([6, 6, 6, 6, 2], 3))
+        self.assertTrue(check_no_choice_predict_correctness([4, 5, 12, 9, 8], 2))
+        self.assertTrue(check_no_choice_predict_correctness([6, 6, 6, 6, 2], 2))
+
+    def test_update_subset_stats_for_without_choice_analysis(self):
+        stats = {
+            "total_examined": 4,
+            "correct_prediction": 5,
+            "total_correct_choice_word_generated": 10,
+        }
+        update_subset_stats_for_without_choice_analysis(stats,  [6, 6, 0, 3, 2], 1)
+        self.assertEqual(stats["total_examined"], 5)
+        self.assertEqual(stats["correct_prediction"], 6)
+        self.assertEqual(stats["total_correct_choice_word_generated"], 16)
+        update_subset_stats_for_without_choice_analysis(stats, [6, 6, 0, 3, 2], 4)
+        self.assertEqual(stats["total_examined"], 6)
+        self.assertEqual(stats["correct_prediction"], 6)
+        self.assertEqual(stats["total_correct_choice_word_generated"], 18)
--- experiments/results_analysis.py
+++ experiments/results_analysis.py
@@ -1,10 +1,12 @@
 import datetime
 import json
+import math
 import pickle
 import spacy
 
 from datasets import load_dataset
 
+from commongen_validation_test_set_generation.language_models import get_language_models
 from experiments.utils import log_progress
 
 nlp = spacy.load("en_core_web_lg")
@@ -41,25 +43,81 @@ def lemmatize(s: str):
 def count_occurences(sentences: list[str], targets: list[str]) -> int:
     cnt = 0
     for target in targets:
-        lemmatized_target = lemmatize(target)
+        lemmatized_target = lemmatize(target).lower()
+        if lemmatized_target == "":
+            continue
         for sentence in sentences:
-            truncated = sentence.split("=")[-1]
-            lemmatized_sentence = lemmatize(truncated)
-            if lemmatized_target in lemmatized_sentence:
-                cnt += 1
+            truncated = sentence
+            lemmatized_sentence = lemmatize(truncated).lower()
+            cnt += lemmatized_sentence.count(lemmatized_target)
     return cnt
+
+
+def get_current_question_choice_status_with_choice_analysis(question: dict, generations: dict) -> dict:
+    choices_stats = {
+        "inclusion_count": [],
+        "avg_sequences_scores": []
+    }
+
+    has_scores = "sequences_scores" in generations
+
+    generated_sentences = generations["sentences"]
+    scores = generations["sequences_scores"] if has_scores else [0] * 20
+    for choice_idx in range(0, 5):
+        choice_content_words = question[f"choice_{choice_idx}_content_words"]
+        choice_generations = generated_sentences[choice_idx * 4:choice_idx * 4 + 4]
+        choice_scores = scores[choice_idx * 4:choice_idx * 4 + 4]
+        inclusion_count = count_occurences(choice_generations, choice_content_words)
+        choices_stats["inclusion_count"].append(inclusion_count)
+        choices_stats["avg_sequences_scores"].append(sum(choice_scores) / len(choice_scores))
+    return choices_stats
+
+
+def get_correct_choice_idx(question: dict) -> int:
+    choice_idx_map = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
+    letter_correct_choice = question["answerKey"]
+    idx_correct_choice = choice_idx_map[letter_correct_choice]
+    return idx_correct_choice
+
+
+def check_inclusion_scores_predict_correctness(idx_correct_choice: int, choices_stats: dict) -> (bool, bool):
+    highest_inclusion_count = max(choices_stats["inclusion_count"])
+    inclusion_predict_correct = choices_stats["inclusion_count"][idx_correct_choice] == highest_inclusion_count
+
+    highest_score = max(choices_stats["avg_sequences_scores"])
+    score_predict_correct = math.isclose(choices_stats["avg_sequences_scores"][idx_correct_choice], highest_score)
+    return inclusion_predict_correct, score_predict_correct
+
+
+def update_subset_stats_for_with_choice_analysis(
+        stats: dict, choices_stats: dict, idx_correct_choice: int
+):
+    (inclusion_predict_correct, score_predict_correct) = check_inclusion_scores_predict_correctness(idx_correct_choice, choices_stats)
+    stats["correct_prediction_by_inclusion_count"] += inclusion_predict_correct
+    stats["correct_prediction_by_sequences_score"] += score_predict_correct
+    stats["avg_correct_prediction_inclusion"] = ((stats["avg_correct_prediction_inclusion"] * stats["total_examined"]) +
+                                                 choices_stats["inclusion_count"][idx_correct_choice]) / (stats["total_examined"] + 1)
+
+    stats["avg_correct_prediction_sequence_score"] = ((stats["avg_correct_prediction_sequence_score"] * stats["total_examined"]) +
+                                                      choices_stats["avg_sequences_scores"][idx_correct_choice]) / (stats["total_examined"] + 1)
+
+    wrong_prediction_inclusion_count = [v for ci, v in enumerate(choices_stats["inclusion_count"]) if ci != idx_correct_choice]
+    wrong_sequence_scores = [v for ci, v in enumerate(choices_stats["avg_sequences_scores"]) if ci != idx_correct_choice]
+
+    stats["avg_wrong_prediction_inclusion"] = ((stats["avg_wrong_prediction_inclusion"] * stats["total_examined"]) + sum(
+        wrong_prediction_inclusion_count) / len(wrong_prediction_inclusion_count)) / (stats["total_examined"] + 1)
+    stats["avg_wrong_prediction_sequence_score"] = ((stats["avg_wrong_prediction_sequence_score"] * stats["total_examined"]) + sum(
+        wrong_sequence_scores) / len(wrong_sequence_scores)) / (stats["total_examined"] + 1)
+
+    stats["total_examined"] += 1
 
 
 def analyze_with_choice_generations(model_name: str):
     dataset = load_dataset("liujqian/commonsenseqa_with_content_words")
     total_questions = len(dataset["train"]) + len(dataset["validation"])
-    choice_idx_map = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
     all_results = {}
     for subset_name in file_postfix_without_choice:
-        with open(
-                f"generated_sentences/{model_name}{file_postfix_with_choice[subset_name]}",
-                "r",
-        ) as file:
+        with open(f"generated_sentences/{model_name}{file_postfix_with_choice[subset_name]}", "r", ) as file:
             model_generations = json.load(file)
         subset = dataset[subset_name]
         stats = {
@@ -73,62 +131,18 @@ def analyze_with_choice_generations(model_name: str):
         }
         for i in range(len(subset["id"])):
             if i % 200 == 0:
-                log_progress(
-                    i,
-                    len(subset["id"]),
-                    10,
-                    f"Analyzing the results of {model_name} on the {subset_name} set. This is WITH choices' content words."
-                )
+                additional_info = f"Analyzing the results of {model_name} on the {subset_name} set. This is WITH choices' content words."
+                log_progress(i, len(subset["id"]), 10, additional_info)
             question_generations = model_generations[str(i)]
             question = subset[i]
-            all_generated_sentences = question_generations["sentences"]
-            all_sequences_scores = question_generations["sequences_scores"] \
-                if "sequences_scores" in question_generations else [0] * 20
-            cur_question_choices_stats = {
-                "inclusion_count": [],
-                "avg_sequences_scores": []
-            }
-            for choice_idx in range(0, 5):
-                cur_choice_content_words = question[f"choice_{choice_idx}_content_words"]
-                all_generations_for_cur_choice = all_generated_sentences[choice_idx * 4:choice_idx * 4 + 4]
-                all_scores_for_cur_choice = all_sequences_scores[choice_idx * 4:choice_idx * 4 + 4]
-                inclusion_count = count_occurences(all_generations_for_cur_choice, cur_choice_content_words)
-                cur_question_choices_stats["inclusion_count"].append(inclusion_count)
-                cur_question_choices_stats["avg_sequences_scores"].append(
-                    sum(all_scores_for_cur_choice) / len(all_scores_for_cur_choice))
+            choices_stats = get_current_question_choice_status_with_choice_analysis(question, question_generations)
+            idx_correct_choice = get_correct_choice_idx(question)
 
-            letter_correct_choice = question["answerKey"]
-            idx_correct_choice = choice_idx_map[letter_correct_choice]
-            stats["correct_prediction_by_inclusion_count"] += \
-                cur_question_choices_stats["inclusion_count"][idx_correct_choice] == max(
-                    cur_question_choices_stats["inclusion_count"]
-                )
-            stats["correct_prediction_by_sequences_score"] += idx_correct_choice == argmax(
-                cur_question_choices_stats["avg_sequences_scores"])
-            stats["avg_correct_prediction_inclusion"] = ((stats["avg_correct_prediction_inclusion"] * stats[
-                "total_examined"]) + cur_question_choices_stats["inclusion_count"][idx_correct_choice]) / (stats[
-                                                                                                               "total_examined"] + 1)
-            stats["avg_correct_prediction_sequence_score"] = ((stats["avg_correct_prediction_sequence_score"] * stats[
-                "total_examined"]) + cur_question_choices_stats["avg_sequences_scores"][idx_correct_choice]) / (stats[
-                                                                                                                    "total_examined"] + 1)
-            wrong_prediction_inclusion_count = [v for ci, v in enumerate(cur_question_choices_stats["inclusion_count"])
-                                                if ci != idx_correct_choice]
-            wrong_sequence_scores = [v for ci, v in enumerate(cur_question_choices_stats["avg_sequences_scores"])
-                                     if ci != idx_correct_choice]
-            stats["avg_wrong_prediction_inclusion"] = ((stats["avg_wrong_prediction_inclusion"] * stats[
-                "total_examined"]) + sum(wrong_prediction_inclusion_count) / len(wrong_prediction_inclusion_count)) / (
-                                                              stats["total_examined"] + 1)
-            stats["avg_wrong_prediction_sequence_score"] = ((stats["avg_wrong_prediction_sequence_score"] * stats[
-                "total_examined"]) + sum(wrong_sequence_scores) / len(wrong_sequence_scores)) / (stats[
-                                                                                                     "total_examined"] + 1)
-            stats["total_examined"] += 1
+            update_subset_stats_for_with_choice_analysis(stats, choices_stats, idx_correct_choice)
         all_results[subset_name] = stats
-    total_inclusion = sum(
-        [all_results[subset_name]["correct_prediction_by_inclusion_count"] for subset_name in
-         all_results])
-    total_score = sum(
-        [all_results[subset_name]["correct_prediction_by_sequences_score"] for subset_name in
-         all_results])
+
+    total_inclusion = sum([all_results[subset_name]["correct_prediction_by_inclusion_count"] for subset_name in all_results])
+    total_score = sum([all_results[subset_name]["correct_prediction_by_sequences_score"] for subset_name in all_results])
     all_results["inclusion_accuracy"] = total_inclusion / total_questions
     all_results["score_accuracy"] = total_score / total_questions
     all_results["inclusion_correct_count"] = total_inclusion
@@ -137,66 +151,72 @@ def analyze_with_choice_generations(model_name: str):
         json.dump(all_results, file)
 
 
+def get_choice_mention_count(question: dict, question_generations: dict) -> list:
+    # choice_content_words_stats = {}
+    # for choice_idx in range(0, 5):
+    #     choice_content_words = question[f"choice_{choice_idx}_content_words"]
+    #     for word in choice_content_words:
+    #         if word not in choice_content_words_stats:
+    #             choice_content_words_stats[word] = {choice_idx}
+    #         else:
+    #             choice_content_words_stats[word].add(choice_idx)
+    # choice_mention_count = [0, 0, 0, 0, 0]
+    # for sentence in question_generations["sentences"]:
+    #     lemmatized_sentence = lemmatize(sentence)
+    #     for choice_content_word in choice_content_words_stats:
+    #         if choice_content_word in lemmatized_sentence:
+    #             for choice_idx in choice_content_words_stats[choice_content_word]:
+    #                 choice_mention_count[choice_idx] += 1 / len(choice_content_words_stats[choice_content_word])
+    # return choice_mention_count
+    choice_mention_count = [0, 0, 0, 0, 0]
+    for choice_idx in range(0, 5):
+        choice_content_words = question[f"choice_{choice_idx}_content_words"]
+        generated_sentences = question_generations["sentences"]
+        mention_count = count_occurences(sentences=generated_sentences, targets=choice_content_words)
+        choice_mention_count[choice_idx] = mention_count
+    return choice_mention_count
+
+
+def check_no_choice_predict_correctness(choice_mention_count: list, idx_correct_choice: int):
+    predicted = choice_mention_count[idx_correct_choice] == max(choice_mention_count)
+    return predicted
+
+
+def update_subset_stats_for_without_choice_analysis(stats: dict, choice_mention_count: list, idx_correct_choice: int):
+    predicted_correct = check_no_choice_predict_correctness(choice_mention_count, idx_correct_choice)
+    stats["correct_prediction"] += predicted_correct
+    stats["total_correct_choice_word_generated"] += choice_mention_count[idx_correct_choice]
+    stats["total_examined"] += 1
+
+
 def analyze_without_choice_generations(model_name: str):
     dataset = load_dataset("liujqian/commonsenseqa_with_content_words")
     total_questions = len(dataset["train"]) + len(dataset["validation"])
     choice_idx_map = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
     all_results = {}
     for subset_name in file_postfix_without_choice:
-        with open(
-                f"generated_sentences/{model_name}{file_postfix_without_choice[subset_name]}",
-                "r"
-        ) as file:
+        with open(f"generated_sentences/{model_name}{file_postfix_without_choice[subset_name]}", "r") as file:
             model_generations = json.load(file)
         subset = dataset[subset_name]
-        # what we need record:
-        # prediction accuracy, percentage of times the correct term is predicted at all,
-        # average correct term shown, percentage of any term shown, average any term shown.
         stats = {
             "total_examined": 0,
             "correct_prediction": 0,
-            "total_questions_correct_choice_word_generated": 0,
-            "total_questions_any_choice_word_generated": 0
+            "total_correct_choice_word_generated": 0,
         }
-        # rule: if a word is only appearing in one choice, then predicting the word is predicting the choice.
-        #       if a word appear in multiple choices, then split the count
         for i in range(len(subset["id"])):
             if i % 200 == 0:
-                log_progress(
-                    i,
-                    len(subset["id"]),
-                    10,
-                    f"Analyzing the results of {model_name} on the {subset_name} set. This is WITHOUT choices' content words."
-                )
+                info = f"Analyzing the results of {model_name} on the {subset_name} set. This is WITHOUT choices' content words."
+                log_progress(i, len(subset["id"]), 10, info)
             question_generations = model_generations[str(i)]
             question = subset[i]
+            idx_correct_choice = get_correct_choice_idx(question)
             # key are all choice content words, values are sets indicating which choices appeared in
-            choice_content_words_stats = {}
-            for choice_idx in range(0, 5):
-                choice_content_words = question[f"choice_{choice_idx}_content_words"]
-                for word in choice_content_words:
-                    if word not in choice_content_words_stats:
-                        choice_content_words_stats[word] = {choice_idx}
-                    else:
-                        choice_content_words_stats[word].add(choice_idx)
-            choice_mention_count = [0, 0, 0, 0, 0]
-            for sentence in question_generations["sentences"]:
-                for choice_content_word in choice_content_words_stats:
-                    if choice_content_word in lemmatize(sentence):
-                        for choice_idx in choice_content_words_stats[choice_content_word]:
-                            choice_mention_count[choice_idx] += 1 / len(
-                                choice_content_words_stats[choice_content_word])
-            predicted_choice_idx = argmax(choice_mention_count)
-            stats["total_examined"] += 1
-            letter_correct_choice = question["answerKey"]
-            stats["correct_prediction"] += predicted_choice_idx == choice_idx_map[letter_correct_choice]
-            stats["total_questions_correct_choice_word_generated"] += choice_mention_count[
-                                                                          choice_idx_map[letter_correct_choice]] > 0
-            stats["total_questions_any_choice_word_generated"] += sum(choice_mention_count) > 0
-        all_results[subset_name] = stats
+            choice_mention_count = get_choice_mention_count(question, question_generations)
 
-    total_correct = sum(
-        [all_results[subset_name]["correct_prediction"] for subset_name in all_results])
+            update_subset_stats_for_without_choice_analysis(stats, choice_mention_count, idx_correct_choice)
+
+        all_results[subset_name] = stats
+    total_correct = sum([all_results[subset_name]["correct_prediction"] for subset_name in all_results])
     all_results["accuracy"] = total_correct / total_questions
     all_results["correct_count"] = total_correct
     with open(f"analysis-results/{model_name}-analysis-results-NO-choice.json", "w") as file:
@@ -204,8 +224,6 @@ def analyze_without_choice_generations(model_name: str):
 
 
 if __name__ == '__main__':
-    for model_name in [
-        "bloomz_1b1"
-    ]:
+    for model_name in get_language_models():
         analyze_with_choice_generations(model_name)
         analyze_without_choice_generations(model_name)
